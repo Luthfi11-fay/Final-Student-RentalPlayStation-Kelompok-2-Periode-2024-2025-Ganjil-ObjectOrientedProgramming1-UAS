a32f1bb8229ee31f05cf0fb4d06450738f7d63c0
user <user@DESKTOP-QD8IN7U>
2024-10-26 16:02:41 +0700

    TUgas pertama

--- OneDrive/Documents/TUgasPBO/src/projectjava1/AnggotaPerpus.java
+++ OneDrive/Documents/TUgasPBO/src/projectjava1/AnggotaPerpus.java
@@ -6,7 +6,7 @@ public abstract class AnggotaPerpus {
     private final int Nim;
     private final int codedosen;
     private final String alamat;
-    private final double denda;  // Dihapus inisialisasi yang salah
+    private final double denda; 
 
     public AnggotaPerpus(String name, int memberId, int Nim, int codedosen, String alamat, double denda) {
         this.name = name;
@@ -17,27 +17,27 @@ public abstract class AnggotaPerpus {
         this.denda = denda; 
     }
 
-    public String getName() {
+    public String getName() { //method Name
         return name;
     }
 
-    public int getMemberId() {
+    public int getMemberId() { // method memberid
         return memberId;
     }
 
-    public int getNim() {
+    public int getNim() { //method Nim
         return Nim; 
     }
 
-    public int getCodeDosen() {  // Perbaikan nama method sesuai konvensi
+    public int getCodeDosen() {  // method nim
         return codedosen;
     }
 
-    public String getAlamat() {  // Perbaikan penulisan
+    public String getAlamat() {  // Method Alamat
         return alamat;
     }
 
-    public double getDenda() {  // Perbaikan method denda
+    public double getDenda() { // metho Denda 
         return denda;
     }
         
